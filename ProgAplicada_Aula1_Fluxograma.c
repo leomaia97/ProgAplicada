8db1ca4f64ae80e8e006d7d007194b3219f30043
Leo Maia <32986455+leomaia97@users.noreply.github.com>
2018-02-26 19:36:46 -0300

    Add files via upload

---
+++ ProgAplicada_Aula1_Fluxograma.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+    int i;
+    float valor[9], valor_procura;
+
+    printf("Entre com 10 Valores\n");
+
+    for(i=0;i<10;i++){
+            printf("\nEntre com o valor: ");
+            scanf("%f",&valor[i]);
+    }
+
+    printf("Qual valor vc quer procurar: ");
+    scanf("%f",&valor_procura);
+    
+    for(i=0;i<10;i++){
+            
+    }
+
+
+
+}
+
